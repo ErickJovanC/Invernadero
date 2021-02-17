c1f6162502ce3cb6ee312b7d9b898b43c0c3c749
erickjovan <erickjovan106@gmail.com>
2021-02-17 16:08:29 -0600

    Creación de Modelo RegistroPropiedadController -mcr y Diseño inicial del Formulario

---
+++ app/Http/Controllers/RegistroPropiedadController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RegistroPropiedad;
+use Illuminate\Http\Request;
+
+class RegistroPropiedadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('registroPropiedad.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RegistroPropiedad  $registroPropiedad
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RegistroPropiedad $registroPropiedad)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RegistroPropiedad  $registroPropiedad
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RegistroPropiedad $registroPropiedad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RegistroPropiedad  $registroPropiedad
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RegistroPropiedad $registroPropiedad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RegistroPropiedad  $registroPropiedad
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RegistroPropiedad $registroPropiedad)
+    {
+        //
+    }
+}
---
+++ resources/views/registroPropiedad/index.blade.php
@@ -0,0 +1,36 @@
+@include('layouts.app')
+<form action="" method="post">
+    @csrf
+    <div class="form-group">
+        <label for="NombrePropiedad">Nombre propiedad</label>
+        <input type="text" name="NombrePropiedad" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="Propietario">Propietario</label>
+        <input type="text" name="Propietario" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="Estado">Estado</label>
+        <input type="text" name="Estado" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="Municipio">Municipio</label>
+        <input type="text" name="Municipio" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="Colonia">Colonia</label>
+        <input type="text" name="Colonia" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="Cultivo1">Cultivo1</label>
+        <input type="text" name="Cultivo1" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="Extension">Extensión (Manzanas)</label>
+        <input type="text" name="Extension" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="Ubicacion">Ubicación</label>
+        <input type="text" name="Ubicacion" class="form-control">
+    </div>
+</form>
