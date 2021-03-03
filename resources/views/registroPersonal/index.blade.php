de4d1d64a1f328bf1f0d1bbaf66e786c79c263c4
erickjovan <erickjovan106@gmail.com>
2021-03-03 15:03:33 -0600

    Igualación del formato de los formularios

--- resources/views/registroPersonal/index.blade.php
+++ resources/views/registroPersonal/index.blade.php
@@ -4,30 +4,32 @@
 <div class="row">
     <h1 class="titulo mb-5 col-12 text-center">Registro Personal</h1>
 
-    <form action="" method="post" enctype="multipart/form-data">
+    <form action="" method="post" enctype="multipart/form-data" class="col-12">
         @csrf
-        <div class="form-group">
-            <label for="Nombre">Nombre</label>
-            <input type="text" name="Nombre" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="Apellidos">Apellidos</label>
-            <input type="text" name="Apellidos" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="Telefono">Teléfono</label>
-            <input type="text" name="Telefono" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="Direccion">Dirección</label>
-            <input type="text" name="Direccion" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="Foto">Foto</label>
-            <input type="file" name="Foto" class="form-control">
-        </div>
-        <div class="form-group">
-            <input type="submit" value="Registrar Propiedad">
+        <div class="row">
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Nombre">Nombre</label>
+                <input type="text" name="Nombre" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Apellidos">Apellidos</label>
+                <input type="text" name="Apellidos" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Telefono">Teléfono</label>
+                <input type="text" name="Telefono" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Direccion">Dirección</label>
+                <input type="text" name="Direccion" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Foto">Foto</label>
+                <input type="file" name="Foto" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Registrar Propiedad">
+            </div>
         </div>
     </form>
 </div>
--- resources/views/registroPropiedad/index.blade.php
+++ resources/views/registroPropiedad/index.blade.php
@@ -5,68 +5,70 @@
     <h1 class="titulo mb-5 col-12 text-center">Registro de propiedad</h1>
     <form action="" method="post" class="col-12">
     @csrf
-        <div class="form-group col-6">
-            <label for="NombrePropiedad">Nombre propiedad</label>
-            <input type="text" name="NombrePropiedad" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="CodigoRegistro">Código de registro</label>
-            <input type="text" name="CodigoRegistro" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Estado">Estado</label>
-            <input type="text" name="Estado" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Municipio">Municipio</label>
-            <input type="text" name="Municipio" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Colonia">Colonia</label>
-            <input type="text" name="Colonia" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Calle">Calle y Número</label>
-            <input type="text" name="Calle" class="form-control">
-        </div>
-        <div class="form-group  col-6">
-            <label for="Cultivo1">Cultivo1</label>
-            <input type="text" name="Cultivo1" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Extension">Extensión (Manzanas)</label>
-            <input type="text" name="Extension" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Cultivo2">Cultivo2</label>
-            <input type="text" name="Cultivo2" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Extension2">Extensión 2 (Manzanas)</label>
-            <input type="text" name="Extension2" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Cultivo3">Cultivo3</label>
-            <input type="text" name="Cultivo3" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Extension3">Extensión 3 (Manzanas)</label>
-            <input type="text" name="Extension3" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Cultivo4">Cultivo4</label>
-            <input type="text" name="Cultivo4" class="form-control">
-        </div>
-        <div class="form-group col-6">
-            <label for="Extension4">Extensión 4 (Manzanas)</label>
-            <input type="text" name="Extension4" class="form-control">
-        </div>
-        <div class="form-group col-12">
-            <label for="Ubicacion">Ubicación</label>
-            <input type="text" name="Ubicacion" class="form-control">
-        </div>
-        <div class="form-group">
-            <input type="submit" value="Registrar Propiedad">
+        <div class="row">
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="NombrePropiedad">Nombre propiedad</label>
+                <input type="text" name="NombrePropiedad" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="CodigoRegistro">Código de registro</label>
+                <input type="text" name="CodigoRegistro" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Estado">Estado</label>
+                <input type="text" name="Estado" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Municipio">Municipio</label>
+                <input type="text" name="Municipio" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Colonia">Colonia</label>
+                <input type="text" name="Colonia" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Calle">Calle y Número</label>
+                <input type="text" name="Calle" class="form-control">
+            </div>
+            <div class="form-group  col-sm-12 col-md-6 mb-5">
+                <label for="Cultivo1">Cultivo1</label>
+                <input type="text" name="Cultivo1" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Extension">Extensión (Manzanas)</label>
+                <input type="text" name="Extension" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Cultivo2">Cultivo2</label>
+                <input type="text" name="Cultivo2" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Extension2">Extensión 2 (Manzanas)</label>
+                <input type="text" name="Extension2" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Cultivo3">Cultivo3</label>
+                <input type="text" name="Cultivo3" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Extension3">Extensión 3 (Manzanas)</label>
+                <input type="text" name="Extension3" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Cultivo4">Cultivo4</label>
+                <input type="text" name="Cultivo4" class="form-control">
+            </div>
+            <div class="form-group col-sm-12 col-md-6 mb-5">
+                <label for="Extension4">Extensión 4 (Manzanas)</label>
+                <input type="text" name="Extension4" class="form-control">
+            </div>
+            <div class="form-group col-12">
+                <label for="Ubicacion">Ubicación</label>
+                <input type="text" name="Ubicacion" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Registrar Propiedad">
+            </div>
         </div>
     </form>
 </div>
