523965bc0973481b1513aa072c2a91aeccd8cb19
erickjovan <erickjovan106@gmail.com>
2021-03-20 16:26:34 -0600

    Migración y validación lista para el registro de secciones

---
+++ app/Http/Controllers/SeccionController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Seccion;
+use Illuminate\Http\Request;
+use App\Models\RegistroPropiedad;
+use Illuminate\Support\Facades\Auth;
+
+class SeccionController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // Se obtienen los valores de la BD filtrados por usuario
+        $huertas = Auth::user()->huertas;
+
+        //Se envian los valores obtenidos a la vista
+        return view('srhigo.seccion')->with('huertas', $huertas);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = request()->validate([
+            'propiedad' => 'required',
+            'nombreSeccion' => 'required'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Seccion  $seccion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Seccion $seccion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Seccion  $seccion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Seccion $seccion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Seccion  $seccion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seccion $seccion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Seccion  $seccion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seccion $seccion)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_03_20_203119_create_seccions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeccionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seccions', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('nombreSeccion', 30);
+            $table->foreignId('propiedad_id')->
+                references('id')->
+                on('registro_propiedads');
+            $table->foreignId('user_id')->
+                references('id')->
+                on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seccions');
+    }
+}
