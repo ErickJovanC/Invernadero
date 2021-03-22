69e46a1d9cb70d6a65242556dd600406fc6a1d5f
erickjovan <erickjovan106@gmail.com>
2021-03-22 11:34:15 -0600

    Se han establecido las rutas para el sitio

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Auth;
 
 class EmpleadoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         //
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\SeccionController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
