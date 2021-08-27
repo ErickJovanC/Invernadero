c8ece00ffc76ab6c480d304f562e0e7357059f99
erickjovan <erickjovan106@gmail.com>
2021-08-27 14:38:31 -0500

    El estado financiero calcula los ingresos y gastos

--- app/Http/Controllers/FinanzasController.php
+++ app/Http/Controllers/FinanzasController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\AplicacionFertilizante;
 use Illuminate\Http\Request;
+use App\Models\calidadPlanta;
+use App\Models\PreparacionSuelo;
+use App\Models\ActividadesCulturale;
+use App\Models\AplicacionPlaguicida;
 use Illuminate\Support\Facades\Auth;
+use App\Models\AplicacionFertilizante;
+use App\Models\CapacitacionPersonal;
+use App\Models\ControlPreventivo;
+use App\Models\ControlPreventivoPlaga;
+use App\Models\CortePlanta;
+use App\Models\Cosecha;
+use App\Models\LimpiezaCanales;
+use App\Models\RegistroRiego;
+use App\Models\RegistroSiembra;
 
 class FinanzasController extends Controller
 {
@@ -17,12 +29,189 @@ class FinanzasController extends Controller
     {
         $gastos = Auth::user()->registroGastos;
         $id = Auth::user()->id;
-        // $fertilizantes = Auth::user()->aplicacionFertilizante;
         $fertilizantes = AplicacionFertilizante::where('user_id', $id )->orderBy('fertilizante_id', 'ASC')->get();
-        // foreach($fertilizantes as $fertilizante){
-        //     dump($fertilizante->fertilizante_id);
-        // }
-        // dd('id: '. $id);
-        return view('srhigo.reporteFinanciero', compact('gastos', 'fertilizantes'));
+        $plaguicidas = AplicacionPlaguicida::where('user_id', $id )->orderBy('plaguicida_id', 'ASC')->get();
+        $actividadesCulturales = ActividadesCulturale::where('user_id', $id )->orderBy('actividad', 'ASC')->get();
+
+        // Recepción de Planta
+        $cont = 0;
+        $recepcionPlanta = CalidadPlanta::where('user_id', $id)->get('costo');
+        $costo = 0;
+        $costoPracticasA = 0;
+        foreach($recepcionPlanta as $calidad){
+            $costo += $calidad->costo;
+            $costoPracticasA += $calidad->costo;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Recepcion de planta',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        // Preparación de suelo
+        $preparacion = PreparacionSuelo::where('user_id', $id)->get('costoOperacion');
+        $costo = 0;
+        foreach($preparacion as $suelo){
+            $costo += $suelo->costoOperacion;
+            $costoPracticasA += $suelo->costoOperacion;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Preparacion de Suelo',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        // Siembra
+        $siembra = RegistroSiembra::where('user_id', $id)->get('costo');
+        $costo = 0;
+        foreach($siembra as $suelo){
+            $costo += $suelo->costo;
+            $costoPracticasA += $suelo->costo;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Siembra',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        // Cosecha
+        $cosecha = Cosecha::where('user_id', $id)->get('costo');
+        $costo = 0;
+        foreach($cosecha as $cosechaItem){
+            $costo += $cosechaItem->costo;
+            $costoPracticasA += $cosechaItem->costo;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Cosecha',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        // Riego
+        $riego = RegistroRiego::where('user_id', $id)->get('costo');
+        $costo = 0;
+        foreach($riego as $riegoItem){
+            $costo += $riegoItem->costo;
+            $costoPracticasA += $riegoItem->costo;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Riego',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        // controlPreventivo de lotes
+        $controlPreventivo = ControlPreventivo::where('user_id', $id)->get('costo');
+        $costo = 0;
+        foreach($controlPreventivo as $control){
+            $costo += $control->costo;
+            $costoPracticasA += $control->costo;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Control Preventivo en Lote de Plantas',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        // controlPreventivo en plantas y arboles
+        $controlPreventivoPlanta = ControlPreventivoPlaga::where('user_id', $id)->get('costo');
+        $costo = 0;
+        foreach($controlPreventivo as $controlP){
+            $costo += $controlP->costo;
+            $costoPracticasA += $controlP->costo;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Control Preventivo en Arboles',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        // Aplicación Plaguicida
+        $AplicacionPlaguicida = AplicacionPlaguicida::where('user_id', $id)->get('costo');
+        $costo = 0;
+        foreach($AplicacionPlaguicida as $plaguicida){
+            $costo += $plaguicida->costo;
+            $costoPracticasA += $plaguicida->costo;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Gasto derivado por la aplicación de Plaguicidas',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        // Limpieza de Canales
+        $LimpiezaCanales = LimpiezaCanales::where('user_id', $id)->get('costo');
+        $costo = 0;
+        foreach($LimpiezaCanales as $canales){
+            $costo += $canales->costo;
+            $costoPracticasA += $canales->costo;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Limpieza de Canales',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        // Control de Podas
+        $CortePlanta = CortePlanta::where('user_id', $id)->get('costo');
+        $costo = 0;
+        foreach($CortePlanta as $corte){
+            $costo += $corte->costo;
+            $costoPracticasA += $corte->costo;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Podas',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        // Capacitación de personal
+        $CapacitacionPersonal = CapacitacionPersonal::where('user_id', $id)->get('costo');
+        $costo = 0;
+        foreach($CapacitacionPersonal as $capacitacion){
+            $costo += $capacitacion->costo;
+            $costoPracticasA += $capacitacion->costo;
+        }
+        if($costo > 0){
+            $gastosDiversos[$cont] = array(
+                'gasto' => 'Capacitación de personal',
+                'costo' => $costo,
+            );
+            $cont++;
+        }
+
+        $cosechas = Cosecha::where('user_id', $id )/* ->orderBy('fertilizante_id', 'ASC') */->get();
+        $kilos = 0;
+        $precio = 0;
+        foreach($cosechas as $cosecha){
+            $kilos += $cosecha->kilos;
+            $precio += $cosecha->precioVenta;
+        }
+        $toneladas = round($kilos/1000, 2);
+        $precioTonelada = round($precio/$toneladas, 2);
+        $precio = round($precio, 2);
+        $cosechas = [$toneladas, $precioTonelada, $precio];
+
+        return view('srhigo.reporteFinanciero', compact('gastos', 'fertilizantes', 'plaguicidas', 'actividadesCulturales', 'gastosDiversos', 'costoPracticasA', 'cosechas'));
     }
 }
--- resources/views/srhigo/reporteFinanciero.blade.php
+++ resources/views/srhigo/reporteFinanciero.blade.php
@@ -3,17 +3,19 @@
 <a href="{{ route('main') }}" class="btn btn-success">Menú</a>
 <div class="row">
     <h1 class="titulos mb-5 col-12 text-center">Reporte Financiero</h1>
-    <div class="alert alert-danger text-center col-12">Esta sección aun no esta finalizada</div>
+    <div class="alert alert-warning text-center col-12">Aun falta poder seleccionar el rango de fechas</div>
+
+
     <div class="col-12">
         <table class="table mb-5 table-responsive-md table-sm">
             <thead class="thead-dark">
                 <tr class="table-active">
-                    <td scope="row">Productor:</td>
+                    <td scope="row" class="text-right">Productor:</td>
                     <td><b>Gustavo Hernandez Ruiz</b></td>
-                    <td></td>
-                    <td scope="row">Mes:</td>
+                    {{-- <td></td> --}}
+                    <td scope="row" class="text-right">Mes:</td>
                     <td><b>Junio</b></td>
-                    <td scope="row">Año:</td>
+                    <td scope="row" class="text-right">Año:</td>
                     <td><b>2021</b></td>
                 </tr>
                 <tr class="text-center">
@@ -23,18 +25,19 @@
                     <th scope="col">Unidad</th>
                     <th scope="col">Costo Unitario (Promedio)</th>
                     <th scope="col">Costo Total</th>
-                    <th scope="col">Fecha</th>
+                    {{-- <th scope="col">Fecha</th> --}}
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <th scope="row" colspan="6">Fertilización</th>
-                    <th>0.00</th>
+                    <th></th>
                 </tr>
                 {{-- Sección de Fertilización  --}}
                 @php 
                     $cont = 1;
                     $id[0] = 0;
+                    $totalFertilizante = 0;
                 @endphp
                 @foreach ($fertilizantes as $fertilizante)
                     @php 
@@ -64,6 +67,7 @@
                 @endforeach
                     
                 @for($x=1 ; $x<=$cont-1 ; $x++)
+                @php $totalFertilizante += $totalAcumulado[$x]; @endphp
                     <tr>
                         <td>{{ $nombre[$x] }}</td>
                         <td class="text-center">{{ $repeticion[$x] }}</td>
@@ -71,218 +75,186 @@
                         <td>KG/LT</td>
                         <td>{{ $promedio[$x] }}</td>
                         <td>{{ $totalAcumulado[$x] }}</td>
-                        <td></td>
+                        {{-- <td></td> --}}
                     </tr>
                 @endfor
-                {{-- Fin sección Fertilización --}}
 
                 <tr>
-                    <th scope="row">Preparación de Terreno</th>
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
-                    <td></td>
-                    <th>0.00</th>
+                    <td class="text-right">Total:</td>
+                    <th>${{ $totalFertilizante }}</th>
                 </tr>
+                {{-- Fin sección Fertilización --}}
+
+                {{-- Sección de Plaguicida  --}}
                 <tr>
-                    <td>Barrecho</td>
+                    <th scope="row">Plaguicidas</th>
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
-                    <td></td>
+                    {{-- <th>0</th> --}}
                 </tr>
+            @php 
+                $cont = 1;
+                $id[0] = 0;
+                $totalPlaguicida = 0;
+            @endphp
+            @foreach ($plaguicidas as $plaguicida)
+                @php 
+                    $id[$cont] = $plaguicida->plaguicida_id;
+                    if ($id[$cont] != $id[$cont-1] ){
+                        $nombre[$cont] = $plaguicida->plaguicida->nombreComercial;
+                        $unidades[$cont] = $plaguicida->dosisAplicada;
+                        $unidadesTotales[$cont] = $unidades[$cont];
+                        $precio[$cont] = $plaguicida->costo;
+                        $total[$cont] = $unidades[$cont] * $precio[$cont];
+                        $totalAcumulado[$cont] = $unidades[$cont] * $precio[$cont];
+                        $repeticion[$cont] = 1;
+                    }
+                    else{
+                        $cont--;
+                        $unidades[$cont] = $plaguicida->unidades;
+                        $unidadesTotales[$cont] += $unidades[$cont];
+                        $precio[$cont] = $plaguicida->precio;
+                        $total[$cont] = $unidades[$cont] * $precio[$cont];
+                        $totalAcumulado[$cont] += $unidades[$cont] * $precio[$cont];
+                        $repeticion[$cont]++;
+                    }
+
+                    $promedio[$cont] = round($totalAcumulado[$cont] / $unidadesTotales[$cont], 2);
+                    $cont++;
+                @endphp
+            @endforeach
+                
+            @for($x=1 ; $x<=$cont-1 ; $x++)
+                @php $totalPlaguicida += $totalAcumulado[$x]; @endphp
                 <tr>
-                    <td>Nivelación</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>{{ $nombre[$x] }}</td>
+                    <td class="text-center">{{ $repeticion[$x] }}</td>
+                    <td>{{ $unidadesTotales[$x] }}</td>
+                    <td>KG/LT</td>
+                    <td>{{ $promedio[$x] }}</td>
+                    <td>{{ $totalAcumulado[$x] }}</td>
+                    {{-- <td></td> --}}
                 </tr>
+            @endfor
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="text-right">Total:</td>
+                <th>${{ $totalPlaguicida }}</th>
+            </tr>
+            {{-- Fin sección Plaguicida --}}
+
+            <tr>
+                <th scope="row">Labores Culturales</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                {{-- <th>0.00</th> --}}
+            </tr>
+            {{-- Actividades Culturales --}}
+
+                @php 
+                    $cont = 1;
+                    $activity[0] = 'Vacio';
+                    $totalLabores = 0;
+                @endphp
+                @foreach($actividadesCulturales as $actividadC)
+                @php 
+                    $activity[$cont] = $actividadC->actividad;
+                    if ( strcmp($activity[$cont], $activity[$cont-1])!==0 ){
+                        $nombre[$cont] = $actividadC->actividad;
+                        $costo[$cont] = $actividadC->costo;
+                        $totalLabores += $actividadC->costo;
+                        $unidades[$cont] = 1;
+                        
+                    }
+                    else {
+                        $cont--;
+                        $costo[$cont] += $actividadC->costo;
+                        $totalLabores += $actividadC->costo;
+                        $unidades[$cont]++;
+                    }
+                    $cont++;
+                    @endphp
+                @endforeach
+
+                @for($x=1 ; $x<=$cont-1 ; $x++)
                 <tr>
+                    <td>{{ $nombre[$x] }}</td>
+                    <td class="text-center">{{ $unidades[$x] }}</td>
                     <td></td>
+                    <td>Sección</td>
                     <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>{{ $costo[$x] }}</td>
+                    {{-- <td></td> --}}
                 </tr>
-                <tr>
-                    <th scope="row">Fertilización</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <th>47,040</th>
-                </tr>
-                <tr>
-                    <td>Fosfato diamónico 18-46</td>
-                    <td>1</td>
-                    <td>250</td>
-                    <td>KG</td>
-                    <td>10.40</td>
-                    <td>2600</td>
-                    <td>2021-06-31</td>
-                </tr>
-                <tr>
-                    <td>Sulfato de amonio</td>
-                    <td>1</td>
-                    <td>250</td>
-                    <td>KG</td>
-                    <td>10.40</td>
-                    <td>2600</td>
-                    <td>2021-06-31</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope="row">Labores Culturales</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <th>29,700</th>
-                </tr>
-                <tr>
-                    <td>Pinchado para crecimiento</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>HA</td>
-                    <td>2500</td>
-                    <td>2500</td>
-                    <td>2021-06-31</td>
-                </tr>
-                <tr>
-                    <td>Eliminacion de llemas axilares</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>HA</td>
-                    <td>2500</td>
-                    <td>2500</td>
-                    <td>2021-06-31</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th scope="row">Riegos</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <th>6,000</th>
-                </tr>
-                <tr>
-                    <td>Riego Lijero</td>
-                    <td>1</td>
-                    <td>4</td>
-                    <td>JOR</td>
-                    <td>500</td>
-                    <td>2000</td>
-                    <td>2021-06-28</td>
-                </tr>
-                <tr>
-                    <td>Riego Lijero</td>
-                    <td>1</td>
-                    <td>4</td>
-                    <td>JOR</td>
-                    <td>500</td>
-                    <td>2000</td>
-                    <td>2021-06-28</td>
-                </tr>
+                @endfor
                 <tr>
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td class="text-right">Total:</td>
+                    <th>{{ $totalLabores }}</th>
                 </tr>
+                {{-- Fin Actividades Culturales --}}
+
+                {{-- Practicas Agricolas --}}
                 <tr>
-                    <th scope="row">Control de Plagas y Maleza</th>
+                    <th scope="row">Practicas Agricolas</th>
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
-                    <th>1,863</th>
+                    {{-- <td></td> --}}
                 </tr>
+                @php $totalPracticasA =0; @endphp
+                @foreach($gastosDiversos as $gasto)
                 <tr>
-                    <td>Hidroxido Cuprico</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>HA</td>
-                    <td>2500</td>
-                    <td>2500</td>
-                    <td>2021-06-31</td>
+                    <td>{{ $gasto['gasto'] }}</td>
+                    <td></td> 
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    @php $totalPracticasA += $gasto['costo']; @endphp
+                    <td>{{ $gasto['costo'] }}</td>
                 </tr>
+                @endforeach
+
                 <tr>
-                    <td>Carbendazim</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>HA</td>
-                    <td>2500</td>
-                    <td>2500</td>
-                    <td>2021-06-31</td>
+                    <th scope="row"></th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="text-right">Total:</td>
+                    <th>{{ $totalPracticasA }}</th>
+                    {{-- <td></td> --}}
                 </tr>
+
+                {{-- Otros Gastos --}}
                 <tr>
-                    <th scope="row">Cosecha</th>
+                    <th scope="row">Otros Gastos</th>
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
-                    <th>1,863</th>
+                    {{-- <td></td> --}}
                 </tr>
-                <tr>
-                    <td>Hidroxido Cuprico</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>HA</td>
-                    <td>2500</td>
-                    <td>2500</td>
-                    <td>2021-06-31</td>
-                </tr>
-                <tr>
-                    <td>Carbendazim</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>HA</td>
-                    <td>2500</td>
-                    <td>2500</td>
-                    <td>2021-06-31</td>
-                </tr>
-                <tr>
-                    <th scope="row">Diversos</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <th></th>
-                </tr>
+                @php $totalGastos = 0; @endphp
                 @foreach ($gastos as $gasto)
                     <tr>        
                         <td>{{ $gasto->concepto->concepto }}</td>
@@ -291,8 +263,28 @@
                         <td></td>
                         <td></td>
                         <td>{{ $gasto->costo }}</td>
+                        @php $totalGastos += $gasto->costo; @endphp
                     </tr>
                 @endforeach
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="text-right">Total:</td>
+                    <th>{{ $totalGastos }}</th>
+                    {{-- <td></td> --}}
+                </tr>
+
+                <tr>
+                    <th>Costo Total</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    @php $totalEgresos = $totalFertilizante+$totalPlaguicida+$totalLabores+$totalPracticasA+$totalGastos; @endphp
+                    <th>${{ $totalEgresos }}</th>
+                </tr>
             </tbody>
         </table>
     </div>
@@ -307,55 +299,35 @@
             </thead>
             <tbody>
                 <tr>
-                    <td scope="row">Rendimiento</td>
-                    <td>6.00</td> 
+                    <td scope="row">Rendimiento (Toneladas)</td>
+                    <td>{{ $cosechas[0]}}</td> 
                 </tr>
                 <tr>
                     <td scope="row">Precio por Tonelada</td>
-                    <td>50,000</td>
+                    <td>{{ $cosechas[1] }}</td>
                 </tr>
                 <tr>
-                    <th scope="row" colspan="2">Total Ingresos</th>
+                    <td scope="row">Costo por Tonelada</td>
+                    @php $costoTonelada = round( $totalEgresos / $cosechas[0], 2 ); @endphp
+                    <td>{{ $costoTonelada }}</td>
                 </tr>
                 <tr>
-                    <td scope="row">Utilidad</td>
-                    <td>188,407</td>
+                    <td>Total Ingresos</td>
+                    <td>{{ $cosechas[2] }}</td>
                 </tr>
                 <tr>
-                    <td scope="row">Benefio / Costo</td>
-                    <td>169%</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-    {{-- Tabla de Rentabilidad --}}
-    <div class="col-12 col-md-6 mt-5">
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="row" colspan="2">Analisis de Rentabilidad</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td scope="row">Rendimiento</td>
-                    <td>6.00</td>
-                </tr>
-                <tr>
-                    <td scope="row">Precio por Tonelada</td>
-                    <td>50,000</td>
-                </tr>
-                <tr>
-                    <th scope="row" colspan="2">Total Ingresos</th>
+                    <td>Total Gastos</td>
+                    <td>{{ $totalEgresos }}</td>
                 </tr>
                 <tr>
                     <td scope="row">Utilidad</td>
-                    <td>188,407</td>
+                    @php $utilidad = $cosechas[2] - $totalEgresos; @endphp
+                    <td>{{ $utilidad }}</td>
                 </tr>
                 <tr>
                     <td scope="row">Benefio / Costo</td>
-                    <td>169%</td>
+                    @php $beneficio = round( 100/$totalEgresos * $utilidad, 2 ); @endphp
+                    <td>{{ $beneficio }}%</td>
                 </tr>
             </tbody>
         </table>
